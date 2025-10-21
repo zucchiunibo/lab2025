876c7e925f75e620c311d0b971390929fe809fca
Simone Zucchi <simone.zucchi2@studio.unibo.it>
2025-10-21 18:48:32 +0200

    3.1 completed (in pochissimo tempo)

--- lab1/root_macro.C
+++ lab1/root_macro.C
@@ -12,61 +12,96 @@ class macro {
   double phi_;
   double b_;
 
-public:
+ public:
   macro(double k = 5.2, double phi = 1.8, double b = 0.2)
-      : k_{k}, phi_{phi}, b_{b} {}
+      : k_{k}
+      , phi_{phi}
+      , b_{b} {
+  }
 
-  TF1 *cos_function() {
-    TF1 *cos = new TF1("Funzione coseno", "(cos([0]*x + [1]))^2 + [2]", 0., 0.6);
-    cos->SetParameters(k_, phi_, b_);
+  TF1* cos_function(double norm = 1.) {
+    // now includes an overall amplitude parameter [3] so the function can be renormalized
+    TF1* cos = new TF1("Funzione coseno", "[3]*((cos([0]*x + [1]))^2 + [2])", 0., 0.6);
+    cos->SetParameters(k_, phi_, b_, norm);
     return cos;
   }
 
-  TGraph *random_generation_graph(int n) {
+  TGraph* random_generation_graph(int n) {
     std::vector<double> vx, vy;
     for (float i{0.f}; i < 0.6f; i += 0.6 / n) {
       double upper_bound = cos_function()->Eval(i);
-      double y = gRandom->Uniform(0., 1.2);
+      double y           = gRandom->Uniform(0., 1.2);
       if (y <= upper_bound) {
         vx.push_back(i);
         vy.push_back(y);
       }
     }
-    TGraph *graph = new TGraph(vx.size(), &vx[0], &vy[0]);
+    TGraph* graph = new TGraph(vx.size(), &vx[0], &vy[0]);
     return graph;
   }
 
-  TH1F *random_generation_hist(int n, int b) {
+  TH1F* random_generation_hist(int n, int b) {
     std::vector<double> vx, vy;
     for (float i{0.f}; i < 0.6f; i += 0.6 / n) {
       double upper_bound = cos_function()->Eval(i);
-      double y = gRandom->Uniform(0., 1.2);
+      double y           = gRandom->Uniform(0., 1.2);
       if (y <= upper_bound) {
         vx.push_back(i);
       }
     }
-    TH1F *hist = new TH1F("hist", "Istogramma Occorrenze", b, 0, 0.6);
+    TH1F* hist = new TH1F("hist", "Istogramma Occorrenze", b, 0, 0.6);
     for (unsigned int i{0}; i < vx.size(); ++i) {
       hist->Fill(vx[i], 1);
     }
     return hist;
   }
 
-  void accordo(int n, int b) {
-    TH1F *hist1 = (TH1F *)(random_generation_hist(n, b)->Clone("hist1"));
+  void accordo(int n = 1000, int b = 50) { // normalizza HIST e TF1
+    TH1F* hist1 = (TH1F*)(random_generation_hist(n, b)->Clone("hist1"));
     hist1->Scale(1. / hist1->Integral()), "width";
+
+    TF1* cos1     = (TF1*)(cos_function()->Clone("cos1"));
+    double cosInt = cos1->Integral(0., 0.6);
+
+    TF1* cosScaled = (TF1*)(cos_function(1 / cosInt)->Clone("cosScaled"));
+    std::cout << "Hist integral: " << hist1->Integral() << "\n";
+    std::cout << "Cos integral: " << cosScaled->Integral(0., 0.6) << "\n";
+
+    std::vector<double> diff, sigma;
+    auto binWidth = 0.6 / b;
+    for (int i = 0; i < b; ++i) {
+      auto cosIntegral = cosScaled->Integral(i, i + binWidth);
+      diff.push_back(cosIntegral - hist1->GetBinContent(i));
+      sigma.push_back(cosIntegral);
+      // std::cout << "Differnenza bin " << i << " " << cosIntegral - hist1->GetBinContent(i) << "\n";
+    }
+
+    double chiSquared;
+
+    for (int i =0; i<b; ++i) {
+      chiSquared += std::pow(diff[i] / std::sqrt(sigma[i]), 2);
+    }
+    
+    std::cout << "Chi quadro: " << chiSquared << "\n";
+
+    TCanvas* c4 = new TCanvas("c4", "Hist scalato", 800, 600);
+    hist1->Draw("HIST");
+    c4->SaveAs("istrogramma_norm.png");
+    TCanvas* c5 = new TCanvas("c5", "Coseno scalato", 800, 600);
+    cosScaled->Draw();
+    c5->SaveAs("cos_scalato.png");
   }
 
   void draw() {
-    TCanvas *c1 = new TCanvas("c1", "Funzione coseno", 800, 600);
+    TCanvas* c1 = new TCanvas("c1", "Funzione coseno", 800, 600);
     cos_function()->Draw();
     c1->SaveAs("grafico.png");
 
-    TCanvas *c2 = new TCanvas("c2", "Estrazione punti", 800, 600);
+    TCanvas* c2 = new TCanvas("c2", "Estrazione punti", 800, 600);
     random_generation_graph(10000)->Draw();
     c2->SaveAs("punti.png");
 
-    TCanvas *c3 = new TCanvas("c3", "Istogramma", 800, 600);
+    TCanvas* c3 = new TCanvas("c3", "Istogramma", 800, 600);
     random_generation_hist(10000, 50)->Draw();
     // cos_function()->Draw();
     c3->SaveAs("istogramma.png");
--- lab1/root_main.cpp
+++ lab1/root_main.cpp
@@ -3,5 +3,6 @@
 int main(int arcg, char** argv) {
     macro test;
     test.draw(); // ciao
+    test.accordo(10000, 100);
     return 0;
 }
