7fc3dff07c70fa8aa4d196ed7a34ab50add23930
Davide <davide.rizzi4@studio.unibo.it>
2025-10-23 09:20:23 +0200

    punto 3.2

--- lab1/root_macro.C
+++ lab1/root_macro.C
@@ -35,33 +35,54 @@ class macro {
     //     vy.push_back(y);
     //   }
     // }
-    //OPPURE
+    // OPPURE
     for (int i{0}; i < n; ++i) {
-       double x = gRandom->Uniform(0., 0.6);
-       double upper_bound = cos_function()->Eval(x);
-       double y = gRandom->Uniform(0., 1.2);
-       if (y <= upper_bound) {
-         vx.push_back(x);
-         vy.push_back(y);
-       }
-     }
+      double x           = gRandom->Uniform(0., 0.6);
+      double upper_bound = cos_function()->Eval(x);
+      double y           = gRandom->Uniform(0., 1.2);
+      if (y <= upper_bound) {
+        vx.push_back(x);
+        vy.push_back(y);
+      }
+    }
     TGraph* graph = new TGraph(vx.size(), &vx[0], &vy[0]);
     return graph;
   }
 
+  // TH1F* random_generation_hist(int n, int b) {
+  //   std::vector<double> vx, vy;
+  //   for (float i{0.f}; i < 0.6f; i += 0.6 / n) {
+  //     double upper_bound = cos_function()->Eval(i);
+  //     double y           = gRandom->Uniform(0., 1.2);
+  //     if (y <= upper_bound) {
+  //       vx.push_back(i);
+  //     }
+  //   }
+  //   TH1F* hist = new TH1F("hist", "Istogramma Occorrenze", b, 0, 0.6);
+  //   for (unsigned int i{0}; i < vx.size(); ++i) {
+  //     hist->Fill(vx[i], 1);
+  //   }
+  //   return hist;
+  // }
+
   TH1F* random_generation_hist(int n, int b) {
-    std::vector<double> vx, vy;
-    for (float i{0.f}; i < 0.6f; i += 0.6 / n) {
-      double upper_bound = cos_function()->Eval(i);
+    std::vector<double> vx;
+    int entries = 0;
+    for (int i = 0; i < n; ++i) {
+      double x           = gRandom->Uniform(0., 0.6);
       double y           = gRandom->Uniform(0., 1.2);
+      double upper_bound = cos_function()->Eval(x);
       if (y <= upper_bound) {
-        vx.push_back(i);
+        vx.push_back(x);
+        entries += 1;
       }
     }
-    TH1F* hist = new TH1F("hist", "Istogramma Occorrenze", b, 0, 0.6);
-    for (unsigned int i{0}; i < vx.size(); ++i) {
-      hist->Fill(vx[i], 1);
-    }
+    std::cout << "entries: " << entries << '\n';
+    static int histCount = 0;
+    TH1F* hist = new TH1F(Form("hist_%d", histCount++), "Istogramma Occorrenze", b, 0, 0.6);
+    for (double val : vx)
+      hist->Fill(val);
+
     return hist;
   }
 
@@ -79,8 +100,8 @@ class macro {
     std::vector<double> diff, sigma;
     auto binWidth = 0.6 / b;
     for (int i = 0; i < b; ++i) {
-      double xlow = hist1->GetBinLowEdge(i+1);
-      double xup = hist1->GetBinLowEdge(i+2);
+      double xlow        = hist1->GetBinLowEdge(i + 1);
+      double xup         = hist1->GetBinLowEdge(i + 2);
       double cosIntegral = cosScaled->Integral(xlow, xup);
       diff.push_back(cosIntegral - hist1->GetBinContent(i));
       sigma.push_back(cosIntegral);
@@ -89,10 +110,10 @@ class macro {
 
     double chiSquared;
 
-    for (int i =0; i<b; ++i) {
+    for (int i = 0; i < b; ++i) {
       chiSquared += std::pow(diff[i] / std::sqrt(sigma[i]), 2);
     }
-    
+
     std::cout << "Chi quadro: " << chiSquared << "\n";
 
     TCanvas* c4 = new TCanvas("c4", "Hist scalato", 800, 600);
@@ -101,6 +122,39 @@ class macro {
     TCanvas* c5 = new TCanvas("c5", "Coseno scalato", 800, 600);
     cosScaled->Draw();
     c5->SaveAs("cos_scalato.png");
+  }
+
+  void rigenerazione_incertezze(int nGenerazioni = 100, int nEventi = 10000, int nBin = 50) {
+    std::vector<TH1F*> histSet;
+
+    for (int i = 0; i < nGenerazioni; ++i) {
+      histSet.push_back(random_generation_hist(nEventi, nBin));
+    }
+
+    std::vector<double> media(nBin, 0.0);
+    std::vector<double> sigma(nBin, 0.0);
+
+    for (int i = 0; i < nBin; ++i) {
+      for (auto& h : histSet)
+        media[i] += h->GetBinContent(i + 1);
+      media[i] /= nGenerazioni;
+    }
+
+    for (int i = 0; i < nBin; ++i) {
+      for (auto& h : histSet)
+        sigma[i] += pow(h->GetBinContent(i + 1) - media[i], 2);
+      sigma[i] = sqrt(sigma[i] / (nGenerazioni - 1));
+    }
+
+    TGraph* gSigma = new TGraph(nBin);
+    for (int i = 0; i < nBin; ++i)
+      gSigma->SetPoint(i, i, sigma[i]); //Argomenti: pos in lista, x, y
+
+    TCanvas* c = new TCanvas("c_sigma", "Incertezze per bin", 800, 600);
+    gSigma->SetTitle("Fluttuazioni bin; Bin; Deviazione standard");
+    gSigma->SetMarkerStyle(20);
+    gSigma->Draw("AP");
+    c->SaveAs("sigma.png");
   }
 
   void draw() {
--- lab1/root_main.cpp
+++ lab1/root_main.cpp
@@ -1,8 +1,10 @@
 #include "root_macro.C"
 
 int main(int arcg, char** argv) {
+    gRandom->SetSeed(0);
     macro test;
     test.draw(); // ciao
     test.accordo(10000, 100);
+    test.rigenerazione_incertezze();
     return 0;
 }
